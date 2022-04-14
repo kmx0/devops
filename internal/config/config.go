f8624253d35ee845d214879cf14f2cebd8a93c65
kmx0 <kamoxa0@yandex.ru>
2022-04-14 19:07:43 +0300

    change 15 to 300 sec

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,10 +11,9 @@ type Config struct {
 	Address        string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
 	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
-	StoreInterval  time.Duration `env:"STORE_INTERVAL" envDefault:"15s"`
+	StoreInterval  time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
 	StoreFile      string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
 	Restore        bool          `env:"RESTORE" envDefault:"true"`
-	Sync           bool
 }
 
 func LoadConfig() Config {
@@ -23,9 +22,6 @@ func LoadConfig() Config {
 	err := env.Parse(&cfg)
 	if err != nil {
 		logrus.Error(err)
-	}
-	if cfg.PollInterval == 0 {
-		cfg.Sync = true
 	}
 	return cfg
 }
