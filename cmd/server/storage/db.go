7f5154c768cfb39e257717ffdc208ae0dd6028df
kmx0 <kamoxa0@yandex.ru>
2022-04-26 11:46:30 +0300

    add _

--- cmd/server/storage/db.go
+++ cmd/server/storage/db.go
@@ -4,11 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 )
 
 func PingDB(urlExample string) bool {
 	// urlExample := "postgres://postgres:postgres@localhost:5432/metrics"
+	logrus.Info(urlExample)
 	db, err := sql.Open("postgres", urlExample)
 	if err != nil {
 		logrus.Error(err)
