62df494626aa4a92604923dfda6d3c89ca6b94f3
kmx0 <kamoxa0@yandex.ru>
2022-04-26 11:38:23 +0300

    add sql

--- cmd/server/storage/db.go
+++ cmd/server/storage/db.go
@@ -1,28 +1,27 @@
 package storage
 
 import (
-	"context"
-	_ "database/sql"
+	"database/sql"
 	"fmt"
-	"os"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/sirupsen/logrus"
 )
 
 func PingDB(urlExample string) bool {
 	// urlExample := "postgres://postgres:postgres@localhost:5432/metrics"
-
-	// host := "127.0.0.1"
-	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, urlExample)
-	// fmt.Sprintf("host=%s port='5432' dbname='metrics' user='postgres' password='postgres' sslmode=disable", host))
-	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	db, err := sql.Open("postgres", urlExample)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		// os.Exit(1)
+		logrus.Error(err)
 		return false
 	}
-	defer conn.Close(context.Background())
-	err = conn.Ping(ctx)
-	return err == nil
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logrus.Error(err)
+		return false
+	}
+
+	fmt.Println("Successfully connected!")
+	return true
 }
